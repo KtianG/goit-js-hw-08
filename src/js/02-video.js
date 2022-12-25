9cd31acbdd645af913361a69791ccc3fe86ae07b
Krystian Guz <krystian.guz@outlook.com>
2022-12-26 00:13:42 +0100

    Second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,27 @@
 import Player from '@vimeo/player';
 
-console.log('cokolwiek');
+//Dodaj do projektu bibliotekę lodash.throttle i zrób tak,
+//aby czas odtwarzania aktualizował się w storage nie częściej niż raz na sekundę.
+import _ from 'lodash';
+
+//Inicjalizuj odtwarzacz w pliku skryptu tak,
+//jak opisano w sekcji pre-existing player,
 const iframe = document.querySelector('iframe#vimeo-player');
-console.log(iframe);
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+///Do przeładowywania strony używaj metody setCurrentTime()
+//aby wznowić odtwarzanie od zapisanego momentu.
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+//Zapisuj czas odtwarzania w local storage.
+//Niech kluczem do storage będzie "videoplayer-current-time".
+const updateTime = event => {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+  console.log(localStorage.getItem('videoplayer-current-time'));
+};
+
+//Zbadaj dokumentację metody on()
+//i zacznij śledzić zdarzenie timeupdate - aktualizacja czasu odtwarzania.
+player.on('timeupdate', _.throttle(updateTime, 1000));
